c6c98399bb519e9b8b9efea2451d6e355e138812
cnscheihing <cnscheihing@uc.cl>
2020-04-28 12:54:59 -0400

    feat: react-bootstrap components for form

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Form } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 export default function SignUpComponent() {
   const [name, setName] = useState('');
@@ -9,15 +9,51 @@ export default function SignUpComponent() {
   const [password, setPassword] = useState('');
   const [address, setAddress] = useState('');
   const [phone, setPhone] = useState('');
-  const [linkedin, setLinkedin] = useState('');
   // TODO: profile picture & CV
 
+  const onHandleSubmit = () => {
+    // TODO: backend call
+    console.log(name);
+  }
+
   return(
-    <Form>
-      <Form.Group controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" onChange={(event)=>console.log(event.target.value)}/>
-      </Form.Group>
-    </Form>
+    <Container>
+      
+      <h1>Regístrate</h1>
+      <Form>
+        <Form.Group>
+          <Form.Control type="name" placeholder="Nombre" onChange={(event)=>setName(event.target.value)}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="rut" placeholder="Rut" onChange={(event)=>setRut(event.target.value)}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="email" placeholder="Correo" onChange={(event)=>setEmail(event.target.value)}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="password" placeholder="Contraseña" onChange={(event)=>setPassword(event.target.value)}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="address" placeholder="Dirección" onChange={(event)=>setAddress(event.target.value)}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Control type="phone" placeholder="Teléfono" onChange={(event)=>setPhone(event.target.value)}/>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Selecciona un tipo de usuario:</Form.Label>
+          <Row>
+            <Col>
+              <Form.Check type="radio" label="Empleador" />
+            </Col>
+            <Col>
+              <Form.Check type="radio" label="Trabajador" />
+            </Col>
+          </Row>
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={onHandleSubmit} block>Registrar</Button>
+         {/* TODO: validation */}
+      </Form>
+      <span><a>¿Ya tienes una cuenta? Ingresa aquí</a></span>
+    </Container>
   )
 }
