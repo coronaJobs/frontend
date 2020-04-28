1c5bc17cea2bcad2f33767d7dcaef1f8c99ffb67
cnscheihing <cnscheihing@uc.cl>
2020-04-28 18:52:53 -0400

    feat: css styles

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 
 export default function SignUpComponent() {
   const [name, setName] = useState('');
@@ -9,6 +8,7 @@ export default function SignUpComponent() {
   const [password, setPassword] = useState('');
   const [address, setAddress] = useState('');
   const [phone, setPhone] = useState('');
+  const [role, setRole] = useState('employer');
   // TODO: profile picture & CV
 
   const onHandleSubmit = () => {
@@ -17,43 +17,44 @@ export default function SignUpComponent() {
   }
 
   return(
-    <Container>
-      
-      <h1>Regístrate</h1>
-      <Form>
-        <Form.Group>
-          <Form.Control type="name" placeholder="Nombre" onChange={(event)=>setName(event.target.value)}/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Control type="rut" placeholder="Rut" onChange={(event)=>setRut(event.target.value)}/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Control type="email" placeholder="Correo" onChange={(event)=>setEmail(event.target.value)}/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Control type="password" placeholder="Contraseña" onChange={(event)=>setPassword(event.target.value)}/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Control type="address" placeholder="Dirección" onChange={(event)=>setAddress(event.target.value)}/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Control type="phone" placeholder="Teléfono" onChange={(event)=>setPhone(event.target.value)}/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Selecciona un tipo de usuario:</Form.Label>
-          <Row>
-            <Col>
-              <Form.Check type="radio" label="Empleador" />
-            </Col>
-            <Col>
-              <Form.Check type="radio" label="Trabajador" />
-            </Col>
-          </Row>
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={onHandleSubmit} block>Registrar</Button>
-         {/* TODO: validation */}
-      </Form>
-      <span><a>¿Ya tienes una cuenta? Ingresa aquí</a></span>
-    </Container>
+    <div class="background">
+      <div class="form-container">
+        <h1>Regístrate</h1>
+        <Form>
+          <Form.Group>
+            <Form.Control type="name" placeholder="Nombre" onChange={(event)=>setName(event.target.value)} className="text-input"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Control type="rut" placeholder="Rut" onChange={(event)=>setRut(event.target.value)} className="text-input"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Control type="email" placeholder="Correo" onChange={(event)=>setEmail(event.target.value)} className="text-input"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Control type="password" placeholder="Contraseña" onChange={(event)=>setPassword(event.target.value)} className="text-input"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Control type="address" placeholder="Dirección" onChange={(event)=>setAddress(event.target.value)} className="text-input"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Control type="phone" placeholder="Teléfono" onChange={(event)=>setPhone(event.target.value)} className="text-input"/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Selecciona un tipo de usuario:</Form.Label>
+            <Row>
+              <Col>
+                <Form.Check type="radio" label="Empleador" checked={role==='employer'} onChange={()=>(setRole('employer'))} />
+              </Col>
+              <Col>
+                <Form.Check type="radio" label="Trabajador" checked={role==='employee'} onChange={()=>(setRole('employee'))} />
+              </Col>
+            </Row>
+          </Form.Group>
+          <Button variant="primary" type="submit" onClick={onHandleSubmit} block className="signup-button">Registrar</Button>
+          {/* TODO: validation */}
+        </Form>
+        <span><a id="login-link">¿Ya tienes una cuenta? Ingresa aquí</a></span>
+      </div>
+    </div>
   )
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../assets/css/signup.css';
 
 import { SignUpComponent } from '../components';
 
