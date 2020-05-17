c31a46c9fafbc571b34de54795a97c3b835c259f
Rafaela Karachon <rakarachon@uc.cl>
2020-05-17 10:35:09 -0400

    :pencil2: fix: alphabetical imports in react components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 // Components son todos los componentes de React usados en la aplicación que manejan lógica
-export { default as SignUpComponent } from './signup';
 export { default as CurrentUser } from './currentUser';
 export { default as LoginComponent } from './login';
 export { default as PostFormComponent } from './postForm';
+export { default as SignUpComponent } from './signup';
