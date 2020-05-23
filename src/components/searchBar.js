6c49eb7d9383c8f9ce27a604de85bfc419af6421
jiaranda <jiaranda@uc.cl>
2020-05-23 14:36:42 -0400

    :construction: datepicker ready

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,6 +1,10 @@
 // React
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, FormControl, Container, Row, Col } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import { DateTime } from "luxon";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 export default function SearchBar(props) {
   const [text, setText] = useState("");
@@ -9,6 +13,34 @@ export default function SearchBar(props) {
   const [fromApplicantLimit, setFromApplicantLimit] = useState(1);
   const [toApplicantLimit, setToApplicantLimit] = useState(10);
   const [ownerId, setOwnerId] = useState();
+
+  const handleSubmit = () => {
+    let sendFromDate;
+    if (fromDate) {
+      sendFromDate = DateTime.fromISO(fromDate.toISOString())
+        .startOf("day")
+        .toISO();
+    } else {
+      sendFromDate = undefined;
+    }
+
+    console.log(sendFromDate);
+
+    let sendToDate;
+    if (toDate) {
+      sendToDate = DateTime.fromISO(toDate.toISOString())
+        .startOf("day")
+        .toISO();
+    } else {
+      sendToDate = undefined;
+    }
+    console.log(sendToDate);
+  };
+
+  // executed when component is updated and mounted
+  useEffect(() => {
+    handleSubmit();
+  });
 
   const handleTextChange = (event) => {
     setText(event.target.value);
@@ -26,8 +58,6 @@ export default function SearchBar(props) {
   };
 
   const handleToApplicantLimitChange = (event) => {
-    setToApplicantLimit(parseInt(event.target.value));
-
     const newValue = event.target.value;
     if (
       newValue < fromApplicantLimit ||
@@ -38,50 +68,93 @@ export default function SearchBar(props) {
     setToApplicantLimit(parseInt(newValue));
   };
 
+  const handleFromDateChange = (event) => {
+    if (toDate) {
+      if (toDate < event) {
+        setToDate(event);
+      }
+    }
+
+    setFromDate(event);
+  };
+
+  const handleToDateChange = (event) => {
+    if (fromDate) {
+      if (fromDate > event) {
+        setFromDate(event);
+      }
+    }
+
+    setToDate(event);
+  };
   return (
     <Form inline className="ml-4">
       <Container>
         <Row className="mb-4">
-          <Form.Group>
-            <Form.Label>
-              {" "}
-              <i className="fas fa-search" />{" "}
-            </Form.Label>
-            <FormControl
-              type="text"
-              placeholder="Título o descripción"
-              onChange={handleTextChange}
-              value={text}
-            />
-          </Form.Group>
+          <Col>
+            <Form.Group>
+              <Form.Label>
+                {" "}
+                <i className="fas fa-search" />{" "}
+              </Form.Label>
+              <FormControl
+                type="text"
+                placeholder="Título o descripción"
+                onChange={handleTextChange}
+                value={text}
+              />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="exampleForm.SelectCustom">
+              <Form.Label className="mr-2">
+                Cantidad de trabajadores entre
+              </Form.Label>
+              <Form.Control
+                as="select"
+                custom
+                onChange={(event) => handleFromApplicantLimitChange(event)}
+                value={fromApplicantLimit}
+              >
+                {Array.from(Array(10).keys()).map((x) => (
+                  <option key={x + 1}>{x + 1}</option>
+                ))}
+              </Form.Control>
+              <Form.Label className="mx-2">y</Form.Label>
+              <Form.Control
+                as="select"
+                custom
+                onChange={(event) => handleToApplicantLimitChange(event)}
+                value={toApplicantLimit}
+              >
+                {Array.from(Array(10).keys()).map((x) => (
+                  <option key={x + 1}>{x + 1}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Col>
         </Row>
-        <Row className="mb-4">
-          <Form.Group controlId="exampleForm.SelectCustom">
-            <Form.Label className="mr-2">
-              Cantidad de trabajadores entre
-            </Form.Label>
-            <Form.Control
-              as="select"
-              custom
-              onChange={(event) => handleFromApplicantLimitChange(event)}
-              value={fromApplicantLimit}
-            >
-              {Array.from(Array(10).keys()).map((x) => (
-                <option key={x + 1}>{x + 1}</option>
-              ))}
-            </Form.Control>
-            <Form.Label className="mx-2">y</Form.Label>
-            <Form.Control
-              as="select"
-              custom
-              onChange={(event) => handleToApplicantLimitChange(event)}
-              value={toApplicantLimit}
-            >
-              {Array.from(Array(10).keys()).map((x) => (
-                <option key={x + 1}>{x + 1}</option>
-              ))}
-            </Form.Control>
-          </Form.Group>
+        <Row>
+          <Col>
+            <Form.Group controlId="exampleForm.SelectCustom">
+              <Form.Label className="mr-2">Fecha inicial</Form.Label>
+              <DatePicker
+                selected={fromDate}
+                onChange={(event) => handleFromDateChange(event)}
+                dateFormat="dd - MM - yyyy"
+              ></DatePicker>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="exampleForm.SelectCustom">
+              <Form.Label className="mr-2">Fecha final</Form.Label>
+              <DatePicker
+                selected={toDate}
+                onChange={(event) => handleToDateChange(event)}
+                dateFormat="dd - MM - yyyy"
+              ></DatePicker>
+            </Form.Group>
+          </Col>
         </Row>
       </Container>
     </Form>
