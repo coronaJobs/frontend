a6733374b2b3a027bf56536b2a91f6350545d744
Rafaela Karachon <rakarachon@uc.cl>
2020-05-23 00:06:57 -0400

    :construction: feat: create user form validation

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,17 +1,21 @@
+// React
 import React, { useState } from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
-import { useMutation, useQuery } from '@apollo/client';
+import { Redirect } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
+// Bootstrap
+import { Button, Col, Form, Row } from 'react-bootstrap';
+
+// Apollo & GraphQL
+import { useMutation, useQuery } from '@apollo/client';
 import { CREATE_USER } from '../graphql/mutations/users';
 import { IS_LOGGED_IN } from '../graphql/queries/inner_queries';
-import { Redirect } from 'react-router-dom';
 
 // Custom Hooks
 import { useLogin } from '../hooks';
  
 
 export default function SignUpComponent() {
-
   const [name, setName] = useState('');
   const [rut, setRut] = useState('');
   const [mail, setMail] = useState('');
@@ -20,7 +24,13 @@ export default function SignUpComponent() {
   const [phone, setPhone] = useState('');
   const [role, setRole] = useState(1);
   const [validationError, setValidationError] = useState('');
+  // Is this state necessary??? (validForm)
+  // const [validForm, setValidForm] = useState(false);
 
+  const { register, handleSubmit, errors, formState } = useForm();
+  // Read the formState before render to subscribe the form state through Proxy
+  const { touched } = formState;
+  console.log(errors);
   const [userCreated, setUserCreated] = useState(false);
   useLogin(mail, password, userCreated);
 
@@ -30,7 +40,7 @@ export default function SignUpComponent() {
   const { isLoggedIn } = data;
 
   const [signUp] = useMutation(CREATE_USER, {
-    onCompleted({ createUser }) {
+    onCompleted() {
       setUserCreated(true);
     },
     onError() {
@@ -39,30 +49,42 @@ export default function SignUpComponent() {
 
   // TODO: obtener roles (lista de roles) (no esta listo en backend)
 
+  // const validateForm = () => {
+  //   if (!name || !rut || !mail || !password || !address || !phone) {
+  //     setValidationError('Debes rellenar todos los campos para poder registrarte.');
+  //     return false;
+  //   } else {
+  //     setValidationError('');
+  //     return true;
+  //   }
+  // }
+  // const validateForm = () => setValidForm(true);
   const validateForm = () => {
-    if (!name || !rut || !mail || !password || !address || !phone) {
-      setValidationError('Debes rellenar todos los campos para poder registrarte.');
-      return false;
-    } else {
-      setValidationError('');
-      return true;
-    }
+    signUp({ variables: { 
+      name: name,
+      rut: rut,
+      mail: mail,
+      password: password,
+      address: address,
+      phone: phone,
+      role: role,
+    }})
   }
 
-  const onHandleSubmit = (event) => {
-    event.preventDefault();
-    if (validateForm()) {
-      signUp({ variables: { 
-        name: name,
-        rut: rut,
-        mail: mail,
-        password: password,
-        address: address,
-        phone: phone,
-        role: role,
-       }})
-    }
-  }
+  // const onHandleSubmit = (event) => {
+  //   event.preventDefault();
+  //   if (validateForm()) {
+  //     signUp({ variables: { 
+  //       name: name,
+  //       rut: rut,
+  //       mail: mail,
+  //       password: password,
+  //       address: address,
+  //       phone: phone,
+  //       role: role,
+  //      }})
+  //   }
+  // }
 
   return(
     !isLoggedIn ? (
@@ -70,24 +92,113 @@ export default function SignUpComponent() {
         <div className="signup-form-container">
           <h1>Regístrate</h1>
           <p id="signup-error-message">{validationError}</p>
-          <Form>
+          <Form onSubmit={handleSubmit(validateForm)}>
             <Form.Group>
-              <Form.Control type="name" placeholder="Nombre" onChange={(event)=>setName(event.target.value)} className="signup-text-input"/>
+              <Form.Control
+                name='name'
+                type='name'
+                ref={register({required: 'Nombre requerido'})}
+                placeholder="Nombre"
+                onChange={(event)=>setName(event.target.value)}
+                className="signup-text-input"
+                isInvalid={(touched.name && name === '') || (errors.name)}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.name ? errors.name.message : null}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
-              <Form.Control type="rut" placeholder="Rut" onChange={(event)=>setRut(event.target.value)} className="signup-text-input"/>
+              <Form.Control
+                name="rut"
+                ref={register({
+                  required: "Rut requerido",
+                  pattern: {
+                    value: /^0*(\d{1,3}(\.?\d{3})*)\-?([\dkK])$/,
+                    message: 'Rut inválido'
+                  }
+                })}
+                type="rut"
+                placeholder="Rut"
+                onChange={(event)=>setRut(event.target.value)}
+                className="signup-text-input"
+                isInvalid={(touched.rut && rut === '') || (errors.rut)}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.rut ? errors.rut.message : null}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
-              <Form.Control type="mail" placeholder="Correo" onChange={(event)=>setMail(event.target.value)} className="signup-text-input"/>
+              <Form.Control
+                name='mail'
+                ref={register({
+                  required: 'Correo requerido',
+                  pattern: {
+                    value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+                    message: 'Correo inválido',
+                  },
+                })}
+                type="mail"
+                placeholder="Correo"
+                onChange={(event)=>setMail(event.target.value)}
+                className="signup-text-input"
+                isInvalid={(touched.mail && mail === '') || (errors.mail)}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.mail ? errors.mail.message : null}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
-              <Form.Control type="password" placeholder="Contraseña" onChange={(event)=>setPassword(event.target.value)} className="signup-text-input"/>
+              <Form.Control
+                name="password"
+                ref={register({
+                  required: 'Contraseña requerida',
+                  pattern: {
+                    value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+                    message: 'Contraseña inválida. Mínimo 8 caracteres, 1 mayúscula, 1 minúscula, 1 número y 1 caracter especial.'
+                  }
+                })}
+                type="password"
+                placeholder="Contraseña"
+                onChange={(event)=>setPassword(event.target.value)}
+                className="signup-text-input"
+                isInvalid={(touched.password && password === '') || (errors.password)}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.password ? errors.password.message : null}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
-              <Form.Control type="address" placeholder="Dirección" onChange={(event)=>setAddress(event.target.value)} className="signup-text-input"/>
+              <Form.Control
+                name="address"
+                ref={register({
+                  required: 'Dirección requerida',
+                })}
+                type="address"
+                placeholder="Dirección"
+                onChange={(event)=>setAddress(event.target.value)}
+                className="signup-text-input"
+              />
             </Form.Group>
             <Form.Group>
-              <Form.Control type="phone" placeholder="Teléfono" onChange={(event)=>setPhone(event.target.value)} className="signup-text-input"/>
+              <Form.Control
+                name="phone"
+                ref={register({
+                  required: 'Teléfono requerido',
+                  pattern: {
+                    // pattern original: ^\+[1-9]{1}[0-9]{3,14}$
+                    value: /^\+?[0-9]{1,11}$/,
+                    message: "Teléfono inválido"
+                  }
+                })}
+                type="phone"
+                placeholder="Teléfono" 
+                onChange={(event)=>setPhone(event.target.value)}
+                className="signup-text-input"
+                isInvalid={(touched.phone && phone === '') || (errors.phone)}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.phone ? errors.phone.message : null}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
               <Form.Label>Selecciona un tipo de usuario:</Form.Label>
@@ -100,7 +211,14 @@ export default function SignUpComponent() {
                 </Col>
               </Row>
             </Form.Group>
-            <Button variant="primary" type="submit" onClick={onHandleSubmit} block className="signup-button">Registrar</Button>
+            <Button
+              variant="primary"
+              block
+              className="signup-button"
+              type="submit"
+            >
+              Registrar
+            </Button>
           </Form>
           <span><a id="signup-login-link" href='/login'>¿Ya tienes una cuenta? Ingresa aquí</a></span>
         </div>
