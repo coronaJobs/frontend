f847757fa45bc0e8927cf5e3cbb987deb66039c2
dfvasquez <dfvasquez@uc.cl>
2020-05-24 15:11:20 -0400

    :lipstick: add some style

--- src/containers/homeUser.js
+++ src/containers/homeUser.js
@@ -1,47 +1,92 @@
-import React from 'react';
+import React from "react";
 
-import { useQuery } from '@apollo/client';
-import { GET_ALL_POSTS } from '../graphql/queries/posts';
-import { Loading } from '../containers';
-import DefaultPicture from '../assets/images/userProfile-default.png';
-import { Spinner, Container, Row, Col, Image } from 'react-bootstrap';
+import { useQuery } from "@apollo/client";
+import { GET_ALL_POSTS } from "../graphql/queries/posts";
+import { GET_USER_PROFILE } from "../graphql/queries/users";
+import { Loading } from "../containers";
+import DefaultPicture from "../assets/images/userProfile-default.png";
+import { Spinner, Container, Row, Col, Image } from "react-bootstrap";
+import { PostFormComponent } from "../components";
 
 function HomeUser(props) {
-    const { data, loading } = useQuery(GET_ALL_POSTS, {
-        fetchPolicy: 'network-only',
-      });
-    if (loading) return <Loading />;
-    const posts = data.getAllPosts;
-    return (
+  let posts, formButton;
+  const { data, loading } = useQuery(
+    props.currentUser.role.name === "employee"
+      ? GET_ALL_POSTS
+      : GET_USER_PROFILE,
+    {
+      fetchPolicy: "network-only",
+      variables:
+        props.currentUser.role.name === "employee"
+          ? null
+          : { id: parseInt(props.currentUser.id) },
+    }
+  );
+  if (loading) return <Loading />;
+  posts =
+    props.currentUser.role.name === "employee"
+      ? data.getUser.posts
+      : data.getAllPosts;
+  return (
+    <div>
+      {loading ? (
+        <Spinner animation="grow" variant="danger" />
+      ) : posts ? (
         <div>
-            {
-                (loading)?
-                <Spinner animation="grow" variant="danger" />
-                :
-                <div>
-                    <Container>
-                        <div className='center margin-container'>
-                            <h2>Ofertas de trabajo</h2>
-                            <p>Revisa las distintas ofertas de trabajo disponibles.</p>
-                        </div>
-                        <Row>
-                            {
-                                posts.map((post, index) =>
-                                    <Col key={'profile-' + index.toString()} className='center'>
-                                        <Image src={DefaultPicture} className='homeUser-post-picture'/>
-                                        <h3>{post.name}</h3>
-                                        <p>{post.description}</p>
-                                        <span><a className="homeUser-application-button strong-color some-space" href='/signup'>Postular</a></span>
-                                    </Col>
-                                )
-                            }
-                        </Row>
-                    </Container>                
-                </div>
-            }
-       
-        </div>   
-    )
-  }
+          <Container>
+            <div className="center margin-container">
+              <h2>
+                {props.currentUser.role.name === "employee"
+                  ? "Ofertas de trabajo"
+                  : "Mis ofertas de trabajo"}
+              </h2>
+              <p>
+                {props.currentUser.role.name === "employee"
+                  ? "Revisa las distintas ofertas de trabajo disponibles."
+                  : "Ofertas de trabajo vigentes."}
+              </p>
+            </div>
+            <Row>
+              {posts.map((post, index) => (
+                <Col key={"profile-" + index.toString()} className="center">
+                  <Image
+                    src={DefaultPicture}
+                    className="homeUser-post-picture"
+                  />
+                  <h3>{post.name}</h3>
+                  <p>{post.description}</p>
+                  <span>
+                    <a
+                      className="homeUser-application-button strong-color some-space"
+                      href="/signup"
+                    >
+                      Postular
+                    </a>
+                  </span>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </div>
+      ) : (
+        <div className="center margin-container">
+          <h2>
+            {props.currentUser.role.name === "employee"
+              ? "Ofertas de trabajo"
+              : "Mis ofertas de trabajo"}
+          </h2>
+          <p>
+            {props.currentUser.role.name === "employee"
+              ? "No hay ofertas de trabajo disponibles actualmente."
+              : "No tienes ofertas de trabajo. Si necesitas añadir ofertas, haz click en 'Publicar oferta de trabajo'."}
+          </p>
+          {props.currentUser.role.name === "employer"
+            ? (formButton = <PostFormComponent userId={props.currentUser.id} />)
+            : null}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default HomeUser;
