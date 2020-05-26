8927b9ec8c62513aa54393734fab2aa2dbf006ba
cnscheihing <cnscheihing@uc.cl>
2020-05-26 18:03:33 -0400

    :recycle: refactor: added prop types validation and linter

--- src/pages/post/show.js
+++ src/pages/post/show.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import PostShowComponent from "../../components/post/show";
 import "../../assets/css/post/show.css";
 
 export default function PostShow(props) {
-  // const post = prop.post ? props.post : null; //TODO hacer query si post no viene en props
-
   let post;
   if (props.location.state) {
     console.log(props.location.state.post);
     post = props.location.state.post;
   } else {
     // TODO: hacer query getPost(postId) cuando backend este listo
+    // let { postId } = useParams(); id para hacer query
     post = {
       name: "Repartidor de mascarillas",
       description:
@@ -24,18 +23,14 @@ export default function PostShow(props) {
       applicants: [{ name: "Luis" }],
     };
   }
-  // let { postId } = useParams();
 
   return <PostShowComponent post={post} />;
 }
 
 PostShow.propTypes = {
-  post: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    applicantLimit: PropTypes.number.isRequired,
-    owner: PropTypes.object.isRequired,
-    state: PropTypes.object.isRequired,
-    applicants: PropTypes.arrayOf(PropTypes.object).isRequired,
+  location: PropTypes.shape({
+    state: PropTypes.shaope({
+      post: PropTypes.object,
+    }).isRequired,
   }).isRequired,
 };
